f1e4bc9f3a77162b59cc5a71d9137776e3298198
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-04-25 11:52:40 -0400

    change install script to python

---
+++ install.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+print("hello world!")
+# dir_here="${HOME}/.alfred-fts"
+#
+# echo "download copy search index data for alfred-python-boto3-ref ..."
+# if ! [ -e "${dir_here}" ]; then
+#     mkdir -p "${dir_here}"
+# fi
+#
+# curl "https://api.github.com/repos/MacHu-GWU/alfred-python-boto3-ref/releases?per_page=1" | jq |
