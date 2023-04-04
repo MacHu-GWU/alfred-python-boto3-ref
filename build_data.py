360f373c7bc100f0e1357f6e27e0b6fc615a65c1
Sanhe Hu <husanhe@gmail.com>
2023-04-04 02:33:39 -0400

    Update build_data.py

--- build_data.py
+++ build_data.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import re
 import json
-from typing import List
+import typing as T
+import dataclasses
 
 from diskcache import Cache
 from pathlib_mate import Path
 
+try:
+    from functools import cached_property
+except:
+    from cached_property import cached_property
+
+import afwf
 import requests
 from bs4 import BeautifulSoup
 
@@ -14,16 +22,23 @@ dataset_name = "boto3"
 
 dir_here = Path.dir_here(__file__)
 dir_cache = Path(dir_here, ".cache")
+path_spec_file_json = Path(dir_here, "spec-file.json")
 
 dir_home = Path.home()
-p_alfred_data = Path(dir_here, f"{dataset_name}.json")
-p_alfred_setting_data = Path(dir_here, f"{dataset_name}-setting.json")
+p_alfred_data = Path(dir_here, f"{dataset_name}-data.json")
+p_alfred_setting = Path(dir_here, f"{dataset_name}-setting.json")
 
 cache = Cache(dir_cache.abspath)
 cache_expire = 24 * 3600
 # ------------------------------------------------------------------------------
 
+
 def get_html_with_cache(url: str) -> str:
+    """
+    Crawl the URL HTML content, store it to the disk cache.
+    :param url: webpage URL
+    :return: HTML
+    """
     if url in cache:
         return cache.get(url)
     else:
@@ -32,91 +47,240 @@ def get_html_with_cache(url: str) -> str:
         return html
 
 
-alfred_data = list()
-
-
-# for each AWS Service page, locate the client section
-class ServiceHomepage:
-    def __init__(self, name, url):
-        self.name = name
-        self.url = url
-
-
-def get_all_service_homepage() -> List[ServiceHomepage]:
+@dataclasses.dataclass
+class AWSService:
     """
-    get all AWS Service boto3 api homepage
+    ServiceHomepage Dataclass
+    :param name: it is the clickable text in the boto3 document homepage sidebar.
+        For example, for Elastic Block Storage service, the url is
+        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ebs.html,
+        the clickable text in the sidebar is "EBS".
+    :param href_name: the last part of the document url.
+        For example, for Elastic Block Storage service, the url is
+        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ebs.html,
+        the last part is "ebs".
+    :param doc_url: the boto3 document url
+    :param service_id: the service id that can be used in boto3.client("${service_id}")
     """
-    service_homepage_list = list()
 
+    name: str = dataclasses.field()
+    href_name: str = dataclasses.field()
+    doc_url: str = dataclasses.field()
+    service_id: str = dataclasses.field()
+
+    @property
+    def name_snake_case(self) -> str:
+        return self.name.lower().replace("-", "_")
+
+    @property
+    def service_id_snake_case(self) -> str:
+        return self.service_id.lower().replace("-", "_")
+
+    @property
+    def service_id_camel_case(self) -> str:
+        return "".join(
+            [word[0].upper() + word[1:] for word in self.name.lower().split("-")]
+        )
+
+
+def crawl_service_page(html: str) -> str:
+    """
+    Given an AWS Service boto3 API documentation webpage HTML,
+    extract the string token that used to create boto3 client.
+    For example, given EBS boto3 API doc at https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ebs.html
+    it returns "ebs", so we can use boto3.client("ebs") to create ebs client.
+    :param html:
+    :return: service id
+    """
+    pattern = re.compile("client = boto3.client\('[\d\D]*'\)")
+    soup = BeautifulSoup(html, "html.parser")
+    div_client = soup.find("div", class_="highlight-default notranslate")
+    service_id = None
+    for line in div_client.text.split("\n"):
+        res = re.findall(pattern, line)
+        if len(res):
+            service_id = res[0].split("'")[1]
+    return service_id
+
+
+def crawl_home_page(html: str):
+    """
+    get all AWS Service boto3 api homepage from the boto3 doc homepage,
+    from its sidebar.
+    """
+    aws_service_list = list()
+    soup = BeautifulSoup(html, "html.parser")
+    ul = soup.find("ul", class_="current")
+    for a in ul.find_all("a", class_="reference internal"):
+        # make sure the link is an aws service link
+        if "#" not in a.attrs["href"]:
+            href_name = a.attrs["href"]
+            name = a.text
+            doc_url = f"https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/{href_name}"
+            aws_service = AWSService(
+                name=name,
+                href_name=href_name,
+                doc_url=doc_url,
+                service_id="",
+            )
+            # print(aws_service)
+            aws_service_list.append(aws_service)
+    return aws_service_list
+
+
+def get_all_aws_service() -> T.List[AWSService]:
+    """
+    get all AWS Service boto3 api homepage from the boto3 doc homepage,
+    from its sidebar.
+    """
     url_available_services = "https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/index.html"
     html = get_html_with_cache(url_available_services)
+    aws_service_list = crawl_home_page(html)
+    return aws_service_list
+
+
+def step1_crawl_spec_file_data():
+    # --------------------------------------------------------------------------
+    # get all AWS Service boto3 api homepage from the boto3 doc homepage,
+    # from its sidebar.
+    # --------------------------------------------------------------------------
+    print("get all AWS service ...")
+    url_available_services = "https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/index.html"
+    html = get_html_with_cache(url_available_services)
+    aws_service_list = crawl_home_page(html)
+    print(f"found {len(aws_service_list)} AWS Services that has boto3 API")
+
+    # --------------------------------------------------------------------------
+    # iterate through all service page, and extract service_id
+    # --------------------------------------------------------------------------
+    print("extract service_id ...")
+    for aws_service in aws_service_list:
+        print(f"working on {aws_service.doc_url} ...")
+        html = get_html_with_cache(aws_service.doc_url)
+        service__id = crawl_service_page(html)
+        aws_service.service_id = service__id
+
+    # --------------------------------------------------------------------------
+    # dump service data
+    # --------------------------------------------------------------------------
+    print("dump service data ...")
+    spec_file_data: T.List[T.Dict[str, str]] = [
+        dataclasses.asdict(aws_service) for aws_service in aws_service_list
+    ]
+    path_spec_file_json.write_text(json.dumps(spec_file_data, indent=4))
+
+
+def get_documents(aws_service: AWSService) -> T.List[T.Dict[str, T.Any]]:
+    html = get_html_with_cache(aws_service.doc_url)
     soup = BeautifulSoup(html, "html.parser")
-    div = soup.find("div", id="available-services")
-    for a in div.find_all("a", class_="reference internal"):
-        # make sure the link is a aws service link
-        if "#" not in a.attrs["href"]:
-            href_service_name = a.attrs["href"]
-            name = a.text
-            url = f"https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/{href_service_name}"
-            service_homepage = ServiceHomepage(name=name, url=url)
-            service_homepage_list.append(service_homepage)
 
-    return service_homepage_list
+    docs = list()
 
-
-service_homepage_list = get_all_service_homepage()
-for service_homepage in service_homepage_list:
-    # service_homepage = ServiceHomepage( # for debug only
-    #     name="EC2",
-    #     url="https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html"
-    # )
-    print(f"working on {service_homepage.url} ...")
-    html = get_html_with_cache(service_homepage.url)
-    soup = BeautifulSoup(html, "html.parser")
-
-    h1 = soup.find("h1")
-    a = h1.find("a")
-
-    div = soup.find("div", id="client")
-    for ul in div.find_all("ul", class_="simple"):
-        for a in ul.find_all("a"):
+    section = soup.find("section", id="client")
+    if section is not None:
+        for a in section.find_all("a", class_="reference internal"):
             try:
                 href = a.attrs["href"]
-                title = a.attrs["title"]
-                if href.split(".")[-1] == title.split(".")[-1]:
-                    method_name = href.split(".")[-1]
-                    url_api = f"{service_homepage.url}{href}"
-                    doc = dict(
-                        title=f"{service_homepage.name}.{method_name}",
-                        url=url_api,
-                        ngram_field=f"{service_homepage.name.lower()} {' '.join(method_name.lower().split('_'))}",
-                    )
-                    alfred_data.append(doc)
+                method_name = a.text.strip()
+                api_url = f"https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/{href}"
+                doc = dict(
+                    title=f"client | {aws_service.service_id}.{method_name}",
+                    url=api_url,
+                    ngram1="client",
+                    ngram2=aws_service.name.lower(),
+                    ngram3=aws_service.service_id.lower(),
+                    ngram4=method_name,
+                    order=1,
+                )
+                # print(doc)
+                docs.append(doc)
             except:
                 pass
 
-alfred_settings_data = {
-    "columns": [
-        {
-            "name": "title",
-            "type_is_store": True
-        },
-        {
-            "name": "url",
-            "type_is_store": True
-        },
-        {
-            "name": "ngram_field",
-            "ngram_minsize": 2,
-            "ngram_maxsize": 10,
-            "type_is_nporngram": True
-        }
-    ],
-    "title_field": "{title}",
-    "subtitle_field": "open {url}",
-    "arg_field": "{url}",
-    "autocomplete_field": "{ngram_field}",
-}
+    section = soup.find("section", id="paginators")
+    if section is not None:
+        for a in section.find_all("a", class_="reference internal"):
+            try:
+                href = a.attrs["href"]
+                method_name = a.text.strip()
+                api_url = f"https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/{href}"
+                doc = dict(
+                    title=f"paginator | {aws_service.service_id}.{method_name}",
+                    url=api_url,
+                    ngram1="pagi",
+                    ngram2=aws_service.name.lower(),
+                    ngram3=aws_service.service_id.lower(),
+                    ngram4=method_name,
+                    order=2,
+                )
+                # print(doc)
+                docs.append(doc)
+            except:
+                pass
 
-p_alfred_data.write_text(json.dumps(alfred_data, indent=4))
-p_alfred_setting_data.write_text(json.dumps(alfred_settings_data, indent=4))
+    return docs
+
+
+def step2_create_fts_data():
+    spec_file_data = json.loads(path_spec_file_json.read_text())
+    aws_service_list = [AWSService(**data) for data in spec_file_data]
+    docs = list()
+    for aws_service in aws_service_list:
+        print(f"working on {aws_service.doc_url} ...")
+        docs.extend(get_documents(aws_service))
+    p_alfred_data.write_text(json.dumps(docs, indent=4))
+
+
+def step3_create_fts_setting():
+    alfred_settings_data = {
+        "fields": [
+            {"name": "title", "type_is_store": True},
+            {"name": "url", "type_is_store": True},
+            {
+                "name": "ngram1",
+                "type_is_ngram": True,
+                "ngram_minsize": 2,
+                "ngram_maxsize": 6,
+            },
+            {
+                "name": "ngram2",
+                "type_is_ngram": True,
+                "ngram_minsize": 2,
+                "ngram_maxsize": 10,
+                "weight": 2.0,
+            },
+            {
+                "name": "ngram3",
+                "type_is_store": True,
+                "type_is_ngram": True,
+                "ngram_minsize": 2,
+                "ngram_maxsize": 10,
+                "weight": 2.0,
+            },
+            {
+                "name": "ngram4",
+                "type_is_store": True,
+                "type_is_ngram": True,
+                "ngram_minsize": 2,
+                "ngram_maxsize": 10,
+                "weight": 1.0,
+            },
+            {
+                "name": "order",
+                "type_is_store": True,
+                "type_is_numeric": True,
+                "is_sortable": True,
+                "is_sort_ascending": True,
+            },
+        ],
+        "title_field": "{title}",
+        "subtitle_field": "open {url}",
+        "arg_field": "{url}",
+        "autocomplete_field": "{ngram3}.{ngram4}",
+    }
+    p_alfred_setting.write_text(json.dumps(alfred_settings_data, indent=4))
+
+
+# step1_crawl_spec_file_data()
+# step2_create_fts_data()
+# step3_create_fts_setting()
